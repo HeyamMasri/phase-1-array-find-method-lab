a09dbe86db21b5e5e88f6089fdeee7052f81c7f7
heyam <heyam976@gmail.com>
2022-11-27 19:40:21 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+
+  ]
+function superbowlWin(record){
+    for (const game of record) {
+        if (game.result==="W"){
+            return game.year
+        }
+    
+    
+
+
+}}
+record.find(superbowlWin)
